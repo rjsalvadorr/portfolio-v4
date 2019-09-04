d654f31c80098887830bfa315af2a0adb8c5a7f1
RJ Salvador <rjsalvadorr@gmail.com>
2019-09-04 08:49:30 -0400

    adding page navigation

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,25 @@
+import Link from 'next/link';
+
 const Index = () => (
-  <div>
-    <p>Hello Next.js</p>
+  <div class="main-menu">
+    <div class="menu-section">
+      <h1 class="menu-section__title">RJ Salvador</h1>
+    </div>
+    <Link href="/code">
+      <div class="menu-section">
+        <h1 class="menu-section__title">Code</h1>
+      </div>
+    </Link>
+    <Link href="/music">
+      <div class="menu-section">
+        <h1 class="menu-section__title">Music</h1>
+      </div>
+    </Link>
+    <Link href="/blog">
+      <div class="menu-section">
+        <h1 class="menu-section__title">Blog</h1>
+      </div>
+    </Link>
   </div>
 );
 
